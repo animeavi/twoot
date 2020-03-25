0f38bf283ea17edd08ad59842755f7be28be9b52
jeancf <jc@noirextreme.com>
2020-03-25 17:40:07 +0100

    Adding twitter-video-downloader module

--- twoot.py
+++ twoot.py
@@ -29,6 +29,8 @@ import datetime, time
 import re
 from mastodon import Mastodon, MastodonError, MastodonAPIError, MastodonIllegalArgumentError
 
+import twitterdl
+
 # Update from https://www.whatismybrowser.com/guides/the-latest-user-agent/
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
